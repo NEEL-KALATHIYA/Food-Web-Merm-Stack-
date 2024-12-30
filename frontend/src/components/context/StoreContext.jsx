08fb4b6fb54e26c19d2dc5b412ba73e074275a27
NEEL-KALATHIYA <kalathiyaneel4@gmail.com>
2024-12-30 16:20:04 +0530

    new

--- frontend/src/components/context/StoreContext.jsx
+++ frontend/src/components/context/StoreContext.jsx
@@ -5,6 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const { cartItems, setcartItem } = useState({});
+  
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
